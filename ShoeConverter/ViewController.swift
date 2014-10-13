92d3da375af98c9b2253243467139ac235ec55bb
max murrell <libuomac@maxs-mbp.home>
2014-10-13 21:29:11 +0100

    Shoeconverter app

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let sizeFromTextField = mensShoeSizeTextField.text
+        let numberFromTextField = sizeFromTextField.toInt()
+        var integerFromTextField = numberFromTextField!
+//
+        
+        
+       
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+        
+        mensConvertedShoeSizeLabel.text = "\(integerFromTextField + conversionConstant)" + " in European Shoe Size"
+        mensConvertedShoeSizeLabel.resignFirstResponder()
+        
+        
+        }
+    
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    
+    @IBAction func
+        
+        convertWomensShoeSizeButtonPressed(sender: UIButton) {
+        let sizeFromTextField = Double ((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+            
+        womensConvertedShoeSizeLabel.resignFirstResponder()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
